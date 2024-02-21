c8925f19baafac24ed20d5b210fdd55b9bf42cd5
Yuriy Yangel <arlosaneq@gmail.com>
2024-02-21 23:39:47 +0700

    Fix little issue with unused themes and non null return type

--- app/src/main/java/com/example/silentmoon/OnBoardingFragment.kt
+++ app/src/main/java/com/example/silentmoon/OnBoardingFragment.kt
@@ -23,7 +23,7 @@ class OnBoardingFragment : Fragment(R.layout.on_boarding_fragment) {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
         _binding = OnBoardingFragmentBinding.inflate(inflater, container, false)
         return binding.root
