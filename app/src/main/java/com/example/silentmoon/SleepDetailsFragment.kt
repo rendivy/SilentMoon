3badb3d7978d84098895c07ed9b35194e61a44e2
Yuriy Yangel <arlosaneq@gmail.com>
2024-02-28 12:33:10 +0700

    Fixed some issue and added new button

--- app/src/main/java/com/example/silentmoon/SleepDetailsFragment.kt
+++ app/src/main/java/com/example/silentmoon/SleepDetailsFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.silentmoon.databinding.SleepDetailsFragmentBinding
@@ -31,7 +32,24 @@ class SleepDetailsFragment(private val titleImageId: Int, private val titleText:
         savedInstanceState: Bundle?
     ): View {
 
+        var isFavourite = false
+
         binding = SleepDetailsFragmentBinding.inflate(inflater, container, false)
+        binding.backButton.setOnClickListener {
+            parentFragmentManager.popBackStack()
+        }
+        binding.favouriteButton.setOnClickListener {
+            isFavourite = !isFavourite
+            val icon = if (isFavourite) R.drawable.fill_favourite_icon else R.drawable.favourite_icon
+            val message = if (isFavourite) R.string.favoured_increased else R.string.favourites_count
+            binding.favouriteButton.setImageResource(icon)
+            binding.favouritesCount.text = getString(message)
+        }
+        binding.downloadButton.setOnClickListener {
+            val message = R.string.listen_increased
+            binding.listenCount.text = getString(message)
+            Toast.makeText(this.context, "Downloaded!", Toast.LENGTH_SHORT).show()
+        }
         binding.relatedRecyclerView.layoutManager = GridLayoutManager(this.context, 2)
         binding.relatedRecyclerView.adapter = viewAdapter
         binding.headerImage.setImageResource(titleImageId)
